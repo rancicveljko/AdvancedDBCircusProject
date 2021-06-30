dcf796c7d45ef2f1eb7801faf50ddaadfe2bd657
Veljko Rancic <veljko.rancic@elfak.rs>
2021-06-30 16:45:20 +0200

    Dodat CirkusWebApi projekat

---
+++ CirkusApp/CirkusApp1/DTOs/Pregledi/VestinaPregled.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirkusApp1.DTOs.Pregledi
+{
+    public class VestinaPregled
+    {
+        public int IdVestine { get; set; }
+        public String Naziv { get; set; }
+        public int GodinaIskustva { get; set; }
+
+        public VestinaPregled()
+        {
+            
+        }
+        public VestinaPregled(int idvest, String naziv, int god)
+        {
+            this.IdVestine = idvest;
+            this.Naziv = naziv;
+            this.GodinaIskustva = god;
+        }
+    }
+}
